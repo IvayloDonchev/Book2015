7485acefd854cd8a2ae9429e7ceae2d001b0c4d7
Ivaylo Donchev <idonchev@hotmail.com>
2016-06-17 15:06:28 +0300

    ертх

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -3,33 +3,66 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//using static System.Console;
-//using static System.Convert;
+using static System.Console;
+using static System.Convert;
 
 namespace ConsoleApplication1
 {
+    enum orientation : byte
+    {
+        north = 1,
+        south = 2,
+        east = 3,
+        west = 4
+    }
+    struct route
+    {
+        public orientation direction;
+        public double distance;
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            double balance, interestRate, targetBalance;
-            Console.Write("What is your current balance?");
-            balance = Convert.ToDouble(Console.ReadLine());
-            Console.Write("What is your current annual interest rate (in %)?");
-            interestRate = 1 + Convert.ToDouble(Console.ReadLine()) / 100.0;
-            Console.Write("What balance would you like to have?");
-            targetBalance = Convert.ToDouble(Console.ReadLine());
-            int totalYears = 0;
-            while (balance < targetBalance)
+            route myRoute;
+            int myDirection = -1;
+            double myDistance;
+            WriteLine("1) North\n2) South\n3) East\n4) West");
+            do
             {
-                balance *= interestRate;
-                ++totalYears;
-            }
-            Console.WriteLine($"In {totalYears} year{(totalYears == 1 ? "" : "s")} " +
-            $"you'll have a balance of {balance}.");
-            if (totalYears == 0)
-                Console.WriteLine("To be honest, you really didn't need to use this calculator.");
-            Console.ReadKey();
+                WriteLine("Select a direction:");
+                myDirection = ToInt32(ReadLine());
+            } while ((myDirection < 1) || (myDirection > 4));
+            WriteLine("Input a distance:");
+            myDistance = ToDouble(ReadLine());
+            myRoute.direction = (orientation)myDirection;
+            myRoute.distance = myDistance;
+            WriteLine($"myRoute specifies a direction of {myRoute.direction} " +
+            $"and a distance of {myRoute.distance}");
+
+            Write("Enter the size of the array: ");
+            int size = Convert.ToInt32(ReadLine());
+            int[] myIntArray = new int[size];
+            size = myIntArray.Length; // По същество същото
+            for (int i = 0; i < size; ++i)
+                myIntArray[i] = (i + 1) * 10;
+            for (int i = 0; i < size; ++i)
+                Write($"{myIntArray[i]} ");
+            WriteLine();
+
+
+            //try
+            //{
+            //    myIntArray[6] = 3;
+            //    WriteLine(myIntArray[6]);
+            //}
+            //catch(IndexOutOfRangeException e)
+            //{
+            //    WriteLine(e.Message);
+            //}
+            
+
+            ReadKey();
         }
     }
 }
