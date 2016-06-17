d565d316965e371e082c5dd3c1f6511e208b17b3
Ivaylo Donchev <idonchev@hotmail.com>
2016-06-17 13:15:52 +0300

    sss

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static System.Console;
-using static System.Convert;
+//using static System.Console;
+//using static System.Convert;
 
 namespace ConsoleApplication1
 {
@@ -13,21 +13,23 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             double balance, interestRate, targetBalance;
-            WriteLine("What is your current balance?");
-            balance = ToDouble(ReadLine());
-            WriteLine("What is your current annual interest rate (in %)?");
-            interestRate = 1 + ToDouble(ReadLine()) / 100.0;
-            WriteLine("What balance would you like to have?");
-            targetBalance = ToDouble(ReadLine());
+            Console.Write("What is your current balance?");
+            balance = Convert.ToDouble(Console.ReadLine());
+            Console.Write("What is your current annual interest rate (in %)?");
+            interestRate = 1 + Convert.ToDouble(Console.ReadLine()) / 100.0;
+            Console.Write("What balance would you like to have?");
+            targetBalance = Convert.ToDouble(Console.ReadLine());
             int totalYears = 0;
-            do
+            while (balance < targetBalance)
             {
                 balance *= interestRate;
                 ++totalYears;
             }
-            while (balance < targetBalance);
-            WriteLine($"In {totalYears} year{(totalYears == 1 ? "" : "s")} you'll have a balance of {balance}.");
-            ReadKey();
+            Console.WriteLine($"In {totalYears} year{(totalYears == 1 ? "" : "s")} " +
+            $"you'll have a balance of {balance}.");
+            if (totalYears == 0)
+                Console.WriteLine("To be honest, you really didn't need to use this calculator.");
+            Console.ReadKey();
         }
     }
 }
