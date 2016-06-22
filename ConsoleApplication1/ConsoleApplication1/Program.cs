8c9f9b49035663c08a6ce0965a25da02b7659c0e
Ivaylo Donchev <idonchev@hotmail.com>
2016-06-22 10:26:28 +0300

    vvv

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -24,50 +24,64 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            route myRoute;
-            int myDirection = -1;
-            double myDistance;
-            WriteLine("1) North\n2) South\n3) East\n4) West");
-            do
+            //    route myRoute;
+            //    int myDirection = -1;
+            //    double myDistance;
+            //    WriteLine("1) North\n2) South\n3) East\n4) West");
+            //    do
+            //    {
+            //        WriteLine("Select a direction:");
+            //        myDirection = ToInt32(ReadLine());
+            //    } while ((myDirection < 1) || (myDirection > 4));
+            //    WriteLine("Input a distance:");
+            //    myDistance = ToDouble(ReadLine());
+            //    myRoute.direction = (orientation)myDirection;
+            //    myRoute.distance = myDistance;
+            //    WriteLine($"myRoute specifies a direction of {myRoute.direction} " +
+            //    $"and a distance of {myRoute.distance}");
+
+            //    Write("Enter the size of the array: ");
+            //    int size = Convert.ToInt32(ReadLine());
+            //    int[] myIntArray = new int[size];
+            //    size = myIntArray.Length; // По същество същото
+            //    for (int i = 0; i < size; ++i)
+            //        myIntArray[i] = (i + 1) * 10;
+            //    foreach(int i in myIntArray)
+            //        Write($"{i} ");
+            //    WriteLine();
+
+            //    double[,] hillHeight = new double[3, 4];
+            //    int[][] jaggedIntArray;
+            //    jaggedIntArray = new int[2][];
+            //    jaggedIntArray[0] = new int[3];
+            //    jaggedIntArray[1] = new int[4];
+
+
+            //    //try
+            //    //{
+            //    //    myIntArray[6] = 3;
+            //    //    WriteLine(myIntArray[6]);
+            //    //}
+            //    //catch(IndexOutOfRangeException e)
+            //    //{
+            //    //    WriteLine(e.Message);
+            //    //}
+
+
+            //    ReadKey();
+
+
+            string myString = "This is a test.";
+            char[] separator = { ' ' };
+            string[] myWords;
+            myWords = myString.Split(separator);
+            foreach (string word in myWords)
             {
-                WriteLine("Select a direction:");
-                myDirection = ToInt32(ReadLine());
-            } while ((myDirection < 1) || (myDirection > 4));
-            WriteLine("Input a distance:");
-            myDistance = ToDouble(ReadLine());
-            myRoute.direction = (orientation)myDirection;
-            myRoute.distance = myDistance;
-            WriteLine($"myRoute specifies a direction of {myRoute.direction} " +
-            $"and a distance of {myRoute.distance}");
-
-            Write("Enter the size of the array: ");
-            int size = Convert.ToInt32(ReadLine());
-            int[] myIntArray = new int[size];
-            size = myIntArray.Length; // По същество същото
-            for (int i = 0; i < size; ++i)
-                myIntArray[i] = (i + 1) * 10;
-            foreach(int i in myIntArray)
-                Write($"{i} ");
-            WriteLine();
-
-            double[,] hillHeight = new double[3, 4];
-            int[][] jaggedIntArray;
-            jaggedIntArray = new int[2][];
-            jaggedIntArray[0] = new int[3];
-            jaggedIntArray[1] = new int[4];
-
-
-            //try
-            //{
-            //    myIntArray[6] = 3;
-            //    WriteLine(myIntArray[6]);
-            //}
-            //catch(IndexOutOfRangeException e)
-            //{
-            //    WriteLine(e.Message);
-            //}
-
-
+                WriteLine($"{word}");
+            }
+            for (int i = myString.Length-1; i >= 0; --i)
+                Write(myString[i]);
+            WriteLine();       
             ReadKey();
         }
     }
