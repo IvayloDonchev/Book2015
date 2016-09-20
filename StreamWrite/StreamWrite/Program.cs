02d070c993cbb0c3c3ce003eed02ed8f8d2c408c
Ivaylo Donchev <idonchev@hotmail.com>
2016-09-20 15:47:18 +0300

    w

--- StreamWrite/StreamWrite/Program.cs
+++ StreamWrite/StreamWrite/Program.cs
@@ -24,6 +24,7 @@ namespace StreamWrite
                 sw.Write("More than that,");
                 sw.Write($" it's {truth.ToString().ToLower()} that C# is fun.");
                 sw.Close();
+               
             }
             catch (IOException e)
             {
@@ -32,6 +33,32 @@ namespace StreamWrite
                 ReadLine();
                 return;
             }
+
+            string line;
+            try
+            {
+               // FileStream aFile = new FileStream("Log.txt", FileMode.Open);
+               // StreamReader sr = new StreamReader(aFile);
+               // line = sr.ReadLine();
+               // // Read data in line by line.
+               // while (line != null)
+               // {
+               //     WriteLine(line);
+               //     line = sr.ReadLine();
+               // }
+               //sr.Close();
+
+                var lines = File.ReadLines("Log.txt");
+                foreach (var l in lines)
+                    WriteLine(l);
+            }
+            catch (IOException e)
+            {
+                WriteLine("An IO exception has been thrown!");
+                WriteLine(e.ToString());
+                return;
+            }
+            ReadKey();
         }
     }
 }
