dda344d3d861d32c8b537be4482bdc0ece429cf7
Ivaylo Donchev <idonchev@hotmail.com>
2016-09-27 08:39:52 +0300

    comm

--- BegVCSharp_20_3_QuerySyntax/BegVCSharp_20_3_QuerySyntax/Program.cs
+++ BegVCSharp_20_3_QuerySyntax/BegVCSharp_20_3_QuerySyntax/Program.cs
@@ -28,16 +28,55 @@ namespace BegVCSharp_20_3_QuerySyntax
             // WriteLine();
             // foreach (string s in res)
             //     WriteLine(s);
-            string[] names = { "Alonso", "Zheng", "Smith", "Jones", "Smythe", "Small", "Ruiz", "Hsieh", "Jorgenson", "Ilyich", "Singh", "Samba", "Fatimah" };
-            var queryResults = names.Where(n => n.StartsWith("S"));
-            
-            WriteLine("Names beginning with S:");
+
+
+
+            //string[] names = { "Alonso", "Zheng", "Smith", "Jones", "Smythe", "Small", "Ruiz", "Hsieh", "Jorgenson", "Ilyich", "Singh", "Samba", "Fatimah" };
+            //var queryResults = names.Where(n => n.StartsWith("S"));
+
+            //WriteLine("Names beginning with S:");
+            //foreach (var item in queryResults)
+            //{
+            //    WriteLine(item);
+            //}
+            //Write("Program finished, press Enter/Return to continue:");
+            //ReadLine();
+
+
+
+            //string[] names = { "Alonso", "Zheng", "Smith", "Jones", "Smythe", "Small", "Ruiz", "Hsieh", "Jorgenson", "Ilyich", "Singh", "Samba", "Fatimah" };
+            //var queryResults = from n in names where n.StartsWith("S") orderby n descending select n;
+            //WriteLine("Names beginning with S ordered alphabetically:");
+            //foreach (var item in queryResults)
+            //{
+            //    WriteLine(item);
+            //}
+            //Write("Program finished, press Enter/Return to continue:");
+            //ReadLine();
+
+
+
+            int[] numbers = GenerateLotsOfNumbers(12045678);
+            var queryResults = from n in numbers
+                               where n < 1000
+                               select n;
+            WriteLine("Numbers less than 1000:");
             foreach (var item in queryResults)
             {
                 WriteLine(item);
             }
             Write("Program finished, press Enter/Return to continue:");
             ReadLine();
+        }
+        private static int[] GenerateLotsOfNumbers(int count)
+        {
+            Random generator = new Random(0);
+            int[] result = new int[count];
+            for (int i = 0; i < count; i++)
+            {
+                result[i] = generator.Next();
+            }
+            return result;
         }
     }
 }
