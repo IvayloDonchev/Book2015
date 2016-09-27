629f17c317037db02921ac4e1bca084db1220d4f
Ivaylo Donchev <idonchev@hotmail.com>
2016-09-27 16:19:19 +0300

    rr

--- BegVCSharp_20_3_QuerySyntax/BegVCSharp_20_3_QuerySyntax/Program.cs
+++ BegVCSharp_20_3_QuerySyntax/BegVCSharp_20_3_QuerySyntax/Program.cs
@@ -56,15 +56,36 @@ namespace BegVCSharp_20_3_QuerySyntax
 
 
 
-            int[] numbers = GenerateLotsOfNumbers(12045678);
-            var queryResults = from n in numbers
-                               where n < 1000
-                               select n;
-            WriteLine("Numbers less than 1000:");
-            foreach (var item in queryResults)
-            {
-                WriteLine(item);
-            }
+            //int[] numbers = GenerateLotsOfNumbers(12045678);
+            //var queryResults = from n in numbers
+            //                   where n < 1000
+            //                   select n;
+            //WriteLine("Numbers less than 1000:");
+            //foreach (var item in queryResults)
+            //{
+            //    WriteLine(item);
+            //}
+            //Write("Program finished, press Enter/Return to continue:");
+            //ReadLine
+
+
+            int[] numbers = GenerateLotsOfNumbers(12345678);
+            WriteLine("Numeric Aggregates");
+            var queryResults =
+                from n in numbers
+                where n > 1000
+                select n
+;
+            WriteLine("Count of Numbers > 1000");
+            WriteLine(queryResults.Count());
+            WriteLine("Max of Numbers > 1000");
+            WriteLine(queryResults.Max());
+            WriteLine("Min of Numbers > 1000");
+            WriteLine(queryResults.Min());
+            WriteLine("Average of Numbers > 1000");
+            WriteLine(queryResults.Average());
+            WriteLine("Sum of Numbers > 1000");
+            WriteLine(queryResults.Sum(n => (long)n));
             Write("Program finished, press Enter/Return to continue:");
             ReadLine();
         }
