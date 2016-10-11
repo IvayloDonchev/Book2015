b7e0678063eef5617bdda1109eeaa19d04d3b230
Ivaylo Donchev <idonchev@hotmail.com>
2016-10-11 16:19:22 +0300

    dgh

---
+++ Ch22Ex01/Ch22Ex01Client/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ch22Ex01Client.ServiceReference1;
+using static System.Console;
+
+namespace Ch22Ex01Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Title = "Ch22Ex01Client";
+            string numericInput = null;
+            int intParam;
+            do
+            {
+                WriteLine("Enter an integer and press enter to call the WCF service.");
+                numericInput = ReadLine();
+            }
+            while (!int.TryParse(numericInput, out intParam));
+            Service1Client client = new Service1Client();
+            WriteLine(client.GetData(intParam));
+            WriteLine("Press an key to exit.");
+            ReadKey();
+        }
+    }
+}
