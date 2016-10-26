00141e9af2d00cc1b4529a69733783a55324fbc1
Ivaylo Donchev <idonchev@hotmail.com>
2016-10-26 12:00:30 +0300

    rfght

---
+++ Database2/Database2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using static System.Console;
+
+
+namespace Database2
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CompanyName { get; set; }
+        [Key] public int CustomerID { get; set; }
+    }
+    public class Address
+    {
+        public string City { get; set; }
+        public string Country { get; set; }
+        [Key] public string CompanyName { get; set; }
+    }
+    public class CustomerContext : DbContext
+    {
+        public CustomerContext() : base("Customers List") { }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new CustomerContext())
+            {
+                Customer customer1 = new Customer { FirstName = "Kim", LastName = "Abercrombie", CompanyName = "Alpine Ski House" };
+                db.Customers.Add(customer1);
+                db.Customers.Add(new Customer { FirstName = "Jeff", LastName = "Hay", CompanyName = "Coho Winery"});
+                db.Customers.Add(new Customer { FirstName = "Charlie", LastName = "Herb", CompanyName = "Alpine Ski House" });
+
+                db.Addresses.Add(new Address { CompanyName = "Alpine Ski House", City = "Berne", Country = "Switzerland" });
+                db.Addresses.Add(new Address { CompanyName = "Coho Winery", City = "San Francisco", Country = "United States" });
+                db.Addresses.Add(new Address { CompanyName = "Trey Research", City = "New York", Country = "United States" });
+                
+                db.SaveChanges();
+
+                var query = from c in db.Customers
+                            orderby c.FirstName, c.LastName
+                            select new { c.FirstName, c.LastName };
+                foreach(var x in query )
+                    WriteLine($"{x.FirstName} {x.LastName}");
+            }
+        }
+    }
+}
