f12606f5d36705a817a27868e66a30fa40c4618d
Ivaylo Donchev <idonchev@hotmail.com>
2018-02-04 15:30:35 +0200

    g

---
+++ ConsoleApplication1/ConsoleApplication1/Circle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Circle
+    {
+        public Circle() => radius = 0;
+        public Circle(int r) => radius = r;
+        //-----------------------------
+        private int radius;
+        public double Area() => Math.PI * radius * radius;
+        public void SetRadius(int r)
+        {
+            if (r > 0)
+                radius = r;
+            else
+                throw new ArgumentOutOfRangeException("Incorrect radius value");
+        }
+    }
+
+}
